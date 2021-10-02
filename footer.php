612380212774fae2324d2e02c472a7355501312f
abhishekdhull21 <21jiabhi@gmail.com>
2021-10-02 20:09:56 +0530

    some new change after attach api on create the category page

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="scripts/request.js"></script>
 <footer class="main-footer">
-    <strong>Copyright &copy; <a href="spicyrasoi.com">SpicyRasoi.com</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0
-    </div>
-  </footer>
+  <strong>Copyright &copy; <a href="spicyrasoi.com">SpicyRasoi.com</a>.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0
+  </div>
+</footer>
