7e6787afde2f019bd0c8493b41cbe21b4cd643d5
AJAY SHEOKAND <ajaysheokand.as@gmail.com>
2021-10-03 11:46:59 +0530

    Product Page 1.1

--- allproduct.php
+++ allproduct.php
@@ -54,7 +54,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0">Add Category</h1>
+              <h1 class="m-0">All Listed Product </h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
