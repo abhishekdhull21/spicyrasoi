839ab33d491d2f381d7b946d2f8fdeb0a8031d6d
abhishekdhull21 <21jiabhi@gmail.com>
2021-10-08 19:38:06 +0530

    login and register without token

--- spicyrasoi/scripts/request.js
+++ spicyrasoi/scripts/request.js
@@ -3,6 +3,7 @@ const constant = {
   url: "http://api.spicyrasoi.com/",
 };
 $(document).ready(function () {
+  //add new category
   $("#btnAddCategory").click(function (e) {
     e.preventDefault();
     const category = $("#addCategoryInput").val();
@@ -108,5 +109,108 @@ $(document).ready(function () {
     });
   });
 
-  console.log($("#dropdownCategory").val());
+  //login userAgent
+  $("#btnLogin").click(function (e) {
+    e.preventDefault();
+
+    const mobile = $("#mobile").val();
+    const password = $("#password").val();
+    console.log("cate: " + mobile);
+    if (mobile == "" || password == "") {
+      alert("Please fill all field");
+    }
+
+    $(document).ajaxSend(() => {
+      $("#btnLogin").prop("disabled", true);
+      $("#btnLogin").html("Logining...");
+    });
+    $.ajax({
+      url: constant.url + "/user/fetch.php",
+      method: "POST",
+      data: JSON.stringify({
+        mobile: mobile,
+        password: password,
+      }),
+      contentType: "application/json",
+      dataType: "json",
+      success: function (result) {
+        console.log(result);
+
+        const json = result;
+        if (json.success) $(location).prop("href", "../../index.php");
+        else swal({ title: "Error Occured", text: json.error, icon: "error" });
+
+        // $("#btnAddCategory").attr("disabled");
+        $("#btnLogin").html("Login");
+      },
+    });
+    $(document).ajaxError((res) => {
+      console.error(res);
+
+      $("#btnLogin").attr("disabled", false);
+      $("#btnLogin").html("Submit");
+    });
+    $(document).ajaxComplete((res) => {
+      $("#btnLogin").attr("disabled", false);
+      $("#btnLogin").html("Submit");
+    });
+  });
+
+  //register userAgent
+  $("#btnRegister").click(function (e) {
+    e.preventDefault();
+    const user = {
+      username: $("#username").val(),
+      mobile: $("#mobile").val(),
+      email: $("#mobile").val(),
+      password: $("#password").val(),
+    };
+    // console.log("cate: " + user);
+    if (
+      user.mobile == "" ||
+      user.password == "" ||
+      user.email == "" ||
+      user.username == ""
+    ) {
+      swal("Warning", "Please fill all field", "warning");
+      return;
+    }
+
+    $(document).ajaxSend(() => {
+      $("#btnRegister").prop("disabled", true);
+      $("#btnRegister").html("Logining...");
+    });
+    $.ajax({
+      url: constant.url + "/user/add.php",
+      method: "POST",
+      data: JSON.stringify(user),
+      contentType: "application/json",
+      dataType: "json",
+      success: function (result) {
+        console.log(result);
+
+        const json = result;
+        if (json.success)
+          swal("Good Job", "You have successfully created account", "success", {
+            button: true,
+          }).then(() => {
+            $(location).prop("href", "./login.php");
+          });
+        else swal({ title: "Error Occured", text: json.error, icon: "error" });
+        console.info(json.success);
+        // $("#btnAddCategory").attr("disabled");
+        $("#btnRegister").html("Submit");
+      },
+    });
+    $(document).ajaxError((res) => {
+      console.error(res);
+
+      $("#btnRegister").attr("disabled", false);
+      $("#btnRegister").html("Submit");
+    });
+    $(document).ajaxComplete((res) => {
+      $("#btnRegister").attr("disabled", false);
+      $("#btnRegister").html("Submit");
+    });
+  });
 });
--- spicyrasoi/user/login.php
+++ spicyrasoi/user/login.php
@@ -1,5 +1,6 @@
+<html lang="en">
 
-<html lang="en"><head>
+<head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>SpicyRasoi</title>
@@ -13,79 +14,84 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
+
 <body class="login-page" style="min-height: 466px;">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="#" class="h1"><b>Spicy</b>Rasoi</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
-
-      <form action="../index.php" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="forgot.php" class="btn btn-block btn-primary">
-           I forgot my password
-        </a>
-        <a href="createuser.php" class="btn btn-block btn-danger">
-           Register a new membership
-        </a>
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="#" class="h1"><b>Spicy</b>Rasoi</a>
       </div>
-      <!-- /.social-auth-links -->
+      <div class="card-body">
+        <p class="login-box-msg">Sign in to start your session</p>
 
-      <!-- <p class="mb-1">
+        <form action="../index.php" method="post">
+          <div class="input-group mb-3">
+            <input type="number" id="mobile" class="form-control" placeholder="Mobile">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" id="password" class="form-control" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div class="icheck-primary">
+                <input type="checkbox" id="remember">
+                <label for="remember">
+                  Remember Me
+                </label>
+              </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" id="btnLogin" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+
+        <div class="social-auth-links text-center mt-2 mb-3">
+          <a href="forgot.php" class="btn btn-block btn-primary">
+            I forgot my password
+          </a>
+          <a href="createuser.php" class="btn btn-block btn-danger">
+            Register a new membership
+          </a>
+        </div>
+        <!-- /.social-auth-links -->
+
+        <!-- <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
       </p>
       <p class="mb-0">
         <a href="register.html" class="text-center">Register a new membership</a>
       </p> -->
+      </div>
+      <!-- /.card-body -->
     </div>
-    <!-- /.card-body -->
+    <!-- /.card -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+  <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../dist/js/adminlte.min.js"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="../scripts/request.js"></script>
 
 
-</body></html>
+</body>
+
+</html>
