4546c4c05ad84cbecf382f533733055d42b02fe8
AJAY SHEOKAND <ajaysheokand.as@gmail.com>
2021-10-29 22:40:58 +0530

    Final 1.0

--- product/add.php
+++ product/add.php
@@ -32,19 +32,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $admin_id = $data['admin_id'] != '' ? $data['admin_id'] : 0;
         $restaurant = $data['restaurant'] != '' ? $data['restaurant'] : 0;
         $gst = $data['gst'] != '' ? $data['gst'] : 0;
+         $gst_price = $data['gst_price'] != '' ? $data['gst_price'] : 0;
         // $hsn = $data['hsn-ceod'] != '' ? $data['hsn-code'] : 0;
         $gst_type =  filter_var($data['gst_type'], FILTER_SANITIZE_STRING);
         $food_type =  filter_var($data['food_type'], FILTER_SANITIZE_STRING);
         $hsn =  filter_var($data['hsn-code'], FILTER_SANITIZE_STRING);
         $product =  filter_var($data['product'], FILTER_SANITIZE_STRING);
         $unit =  filter_var($data['unit-name'], FILTER_SANITIZE_STRING);
-<<<<<<< HEAD
-        if ($result = mysqli_query($con, "SELECT product_name FROM `product` where product_name = '$product' AND sub_category='$subcategory' AND restaurant='$restaurant' AND category='$category'")) {
-=======
-        if ($result = mysqli_query($con, "SELECT product_name FROM `product` where restaurant = $restaurant and category = $category and product_name = '$product'")) {
->>>>>>> 785535e4255976ab38577e474bdb16e3c4b682b6
+        if ($result = mysqli_query($con, "SELECT product_name FROM `product` where restaurant = $restaurant and category = $category and sub_category=$subcategory and product_name = '$product'")) {
             if (mysqli_num_rows($result) < 1) {
-                $sql = "INSERT INTO `product`( `category`,admin_id,restaurant,`sub_category`, `product_name`, `store_price`, `swiggy_price`, `zomato_price`, `local_price`,`gst_type`,`food_type`, `gst`, `discount`, `unit_name`, `hsn_code`) VALUES($category,$admin_id,$restaurant,$subcategory,'$product',$store_price,$swiggy_price,$zomato_price,$local_price,'$gst_type','$food_type','$gst',$discount,'$unit','$hsn')";
+                $sql = "INSERT INTO `product`( `category`,admin_id,restaurant,`sub_category`, `product_name`, `store_price`, `swiggy_price`, `zomato_price`, `local_price`,`gst_type`,`food_type`, `gst`,`gst_price`, `discount`, `unit_name`, `hsn_code`) VALUES($category,$admin_id,$restaurant,$subcategory,'$product',$store_price,$swiggy_price,$zomato_price,$local_price,'$gst_type','$food_type','$gst','$gst_price',$discount,'$unit','$hsn')";
 
 
                 if ($result =  mysqli_query($con, $sql)) {
