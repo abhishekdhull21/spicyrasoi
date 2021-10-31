c1897084235a0d62bba315c9611d8dc72a3e6778
abhishekdhull21 <21jiabhi@gmail.com>
2021-10-31 16:18:37 +0530

    clean head after wrong merge

--- spicyrasoi/logininfo.php
+++
@@ -1,4 +0,0 @@
-<script>
-    const admin_id = <?php echo $admin_id; ?>;
-    const restaurant = <?php echo $restaurant; ?>;
-</script>
--- spicyrasoi/scripts/constant.js
+++
@@ -1,5 +0,0 @@
-constant = {
-  // url: "http://api.spicyrasoi.com/",
-    url: "http://localhost/apidevelopment/",
-  //url: "http://localhost/projects/spicyrasoi/website/spicyrasoi/",
-};
