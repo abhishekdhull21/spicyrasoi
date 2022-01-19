fdb693a47f542b0759ea3a1531af69c92dfdceb9
abhishekdhull21 <21jiabhi@gmail.com>
2022-01-19 13:30:56 +0530

    stock update and user/data.php file some changes

--- stock/add.php
+++ stock/add.php
@@ -10,34 +10,37 @@ define('ROOTPATH', dirname(__FILE__));
 // $file = fopen("..logs/" . date("d-m-y"). ".text", "a");
 // fwrite($file,("stock/add.php,".file_get_contents('php://input')."\n"));
 
-$response ="";
-if($_SERVER['REQUEST_METHOD']=== 'POST'){
+$response = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //The request using POST method
     header("Content_Type:application/json");
-    $data = json_decode(file_get_contents('php://input'),true);
+    $data = json_decode(file_get_contents('php://input'), true);
     // Condition to check request in JSON
-    if(isset($data['product_id']) && isset($data['in_out']) && isset($data['qty']) && isset($data['restaurant']) && isset($data['admin_id'])){
-        $product_id = $data['product_id'] != '' ? $data['product_id'] : 0 ;
-        $in_out = $data['in_out'] != '' ? $data['in_out'] : 0 ;
-        $qty = $data['qty'] != '' ? $data['qty'] : 0 ;
-        $restaurant = $data['restaurant'] != '' ? $data['restaurant'] : 0 ;
-        $admin_id = $data['admin_id'] != '' ? $data['admin_id'] : 0 ;
-
+    if (isset($data['product_id']) && isset($data['in_out']) && isset($data['qty']) && isset($data['restaurant']) && isset($data['admin_id'])) {
+        $product_id = $data['product_id'] != '' ? $data['product_id'] : 0;
+        $in_out = $data['in_out'] != '' ? $data['in_out'] : 0;
+        $qty = $data['qty'] != '' ? $data['qty'] : 0;
+        $restaurant = $data['restaurant'] != '' ? $data['restaurant'] : 0;
+        $admin_id = $data['admin_id'] != '' ? $data['admin_id'] : 0;
+        $sql = "SELECT stock_id FROM `stock`where restaurant = $restaurant and product_id = $product_id";
+        $res = mysqli_query($con, $sql);
         $sql = "INSERT INTO `stock`(`restaurant`, `admin_id`, `product_id`, `in_out`, `qty`) VALUES ($restaurant, $admin_id, '$product_id', '$in_out', $qty)";
-        $res = mysqli_query($con,$sql);
-        if($res){
+        if (mysqli_num_rows($res) > 0) {
+            $stock_id = mysqli_fetch_assoc($res)['stock_id'];
+            $sql = "UPDATE `stock` SET qty = qty + $qty where stock_id = $stock_id and restaurant = $restaurant and product_id = $product_id";
+        }
+        $res = mysqli_query($con, $sql);
+        if ($res) {
             $response = array(
                 "success" => true,
 
                 "error" => ""
             );
-        }else{
+        } else {
             $err = mysqli_error($con);
         }
-
-    }
-    else {
-        $err= " set key as -> `restaurant`, `admin_id`, `product_id`, `in_out`, `qty`";
+    } else {
+        $err = " set key as -> `restaurant`, `admin_id`, `product_id`, `in_out`, `qty`";
     }
 } else {
     $err = " Header Should be POST";
--- user/data.php
+++ user/data.php
@@ -26,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // if(strpos($content_type, "application/json") !== false){
 
     //     $category =  filter_var($data['category'], FILTER_SANITIZE_STRING);
+
     $sql = "SELECT `user_id`as userid,
         `user_name` as username,
         `user_mobile`as mobile,
@@ -33,6 +34,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         `user_sex` as sex,
         `user_dob` as dob,
         `user_address` as address, `join_on`, `email_verified`, `mobile_verified`, `user_verified`, `user_status` FROM `users` WHERE 1";
+    if (isset($data['getwithrestaurant']) && isset($data['restaurant']) && $data['getwithrestaurant'] == true) {
+        $restaurant = $data['restaurant'];
+        $sql = "SELECT `user_id`as userid,
+        `user_name` as username,
+        `user_mobile`as mobile,
+        `user_email` as email,
+        `user_sex` as sex,
+        `user_dob` as dob,
+        `user_address` as address, `join_on`, `email_verified`, `mobile_verified`, `user_verified`, `user_status` FROM `users` WHERE restaurant = $restaurant";
+    }
     if ($result = mysqli_query($con, $sql)) {
         if (mysqli_num_rows($result) > 0) {
 
