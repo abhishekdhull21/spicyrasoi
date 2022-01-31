3790bee253fd4a4c4c74c1485e68f26da1b55fb9
abhishekdhull21 <21jiabhi@gmail.com>
2022-01-31 16:13:42 +0530

    online paymenttype and max sold product api added

---
+++ analytics/maxsaleproducts.php
@@ -0,0 +1,55 @@
+
+<?php
+require_once('../config.php');
+require_once('../functions.php');
+$err = "";
+$pass = "";
+$token = "";
+$device_type = "";
+$status = true;
+define('ROOTPATH', dirname(__FILE__));
+
+// if (isset(getallheaders()['Device-Type']))
+//     $device_type = getallheaders()['Device-Type'];
+
+// $content_type = (getallheaders());
+$file = fopen("../logs/" . date("d-m-y") . ".txt", "a");
+
+fwrite($file, (date('H:i:s',) . "  analytics/maxsaleproducts.php," . file_get_contents('php://input') . "\n"));
+
+$response = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // The request is using the POST method
+    header("Content-Type:application/json");
+    $data = json_decode(file_get_contents('php://input'), true);
+    // print_r($data);
+    // condition to check request in json 
+    // if(strpos($content_type, "application/json") !== false){
+    if (isset($data['start'], $data['end'], $data['restaurant'])) {
+        $start = $data['start'];
+        $end = $data['end'];
+        $restaurant = $data['restaurant'];
+        //    $start = $data['start'];
+        //     $category =  filter_var($data['category'], FILTER_SANITIZE_STRING);
+
+        if ($result = mysqli_query($con, "SELECT SUM(a.subtotal) as total, Sum(a.qty) as total_qty ,a.product_id ,b.product_name,a.date FROM orders_product a, product b WHERE a.date BETWEEN date('$start') and date('$end')  and a.product_id = b.product_id and b.restaurant = $restaurant  group by a.product_id ORDER BY total deSC LIMIT 5")) {
+            if (mysqli_num_rows($result) > 0) {
+
+                $response = array(
+                    "success" => true,
+                    "data" => mysqli_fetch_all($result, MYSQLI_ASSOC),
+                    "error" => ""
+                );
+            } else {
+                $err = "No data found";
+            }
+        } else {
+            $err = mysqli_error($con);
+        }
+    } else {
+        $err = "set key as -> `admin`,`start`,`end` and `restaurant` ";
+    }
+} else {
+    $err = "Header should be POST";
+}
+sendPostRes($response, $err);
